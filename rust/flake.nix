d1690017dc4260c88cca1dff5867d8a378f9523d
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-31 00:28:04 +1000

    feat: rust flake init

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,26 +1,40 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, rust-overlay, ... }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
+      overlays = [ (import rust-overlay) ];
       perSystem = {
         pkgs,
         config,
         lib,
         ...
       }: 
+      let 
+      # Required Frameworks needed for macOS
+      inherit (pkgs.darwin.apple_sdk.frameworks);
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rustfmt" "cargo" "clippy" "rustc"]; # Programs
+        targets = [""]; # Targets
+      };
+      in
       {
         devShells.default = pkgs.mkShell
         {
-          packages = with pkgs; [
-            rustc
-            rust-analyzer
-            cargo
-            clippy
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ] ++ [
+            rustToolchain
+          ] ++ lib.optionals stdenv.isLinux [
+            openssl
+            # Additional Dependencies here for Linux.
+          ] ++ lib.optionals stdenv.isDarwin [
+            # Frameworks here
           ];
         };
       };
