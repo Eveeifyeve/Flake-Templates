ce2fc74778ed36554dbb705e221442403ff444ac
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-31 02:03:04 +1000

    feat: nodejs flake template

---
+++ node/nix/exampleNpmPackage.nix
@@ -0,0 +1,27 @@
+{ lib, buildNpmPackage, fetchFromGitHub }:
+
+buildNpmPackage rec {
+  pname = "name";
+  version = "1.1.0";
+
+  src = fetchFromGitHub {
+    owner = "";
+    repo = pname;
+    rev = "v${version}";
+    hash = lib.fakeHash;
+  };
+
+  npmDepsHash = lib.fakeHash;
+
+  # The prepack script runs the build script, which we'd rather do in the build phase.
+  # npmPackFlags = [ "--ignore-scripts" ];
+
+  # NODE_OPTIONS = "--openssl-legacy-provider";
+
+  meta = with lib; {
+    description = "";
+    homepage = "url";
+    license = licenses.unlicense;
+    maintainers = [];
+  };
+}
---
+++ node/nix/pnpmExample.nix
@@ -0,0 +1,26 @@
+{stdenv, lib, pnpm_9, fetchFromGitHub, nodejs}:
+let
+  pnpm = pnpm_9;
+in
+stdenv.mkDerivation rec {
+  pname = "name";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "owner";
+    repo = pname;
+    rev = "v${version}";
+    hash = lib.fakeHash;
+  };
+
+  pnpmDeps = pnpm.fetchDeps {
+    inherit pname src version;
+    hash = lib.fakeHash;
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    pnpm.configHook
+  ];
+
+}
