006d5bd28a87f7a92011806f73da0a63e9cce24b
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-31 01:07:59 +1000

    feat: working rust nix template

--- rust/flake.nix
+++ rust/flake.nix
@@ -22,7 +22,7 @@
         _module.args.pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            (import inputs.rust-overlay.overlays.default)
+            (import inputs.rust-overlay)
           ];
         };
 
@@ -41,8 +41,6 @@
           nativeBuildInputs = with pkgs; [
             pkg-config
             toolchain
-          ] ++ [
-            rustToolchain
           ] ++ lib.optionals stdenv.isLinux [
             openssl
             # Additional Dependencies here for Linux.
