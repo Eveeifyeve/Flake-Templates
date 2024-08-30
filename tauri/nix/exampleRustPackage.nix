c757d4653fce4c06696d7924c37980eba1e265ec
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-08-31 01:50:19 +1000

    feat: example package when cargo tauri hook comes out

---
+++ tauri/nix/exampleRustPackage.nix
@@ -0,0 +1,46 @@
+{ lib, fetchFromGitHub, rustPlatform, cargo-tauri, pkg-config, openssl, glib-networking, libsoup, webkitgtk, darwin, stdenv }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "name";
+  version = "12.1.1";
+
+  src = fetchFromGitHub {
+    owner = "";
+    repo = pname;
+    rev = version;
+    hash = lib.fakeHash;
+  };
+
+  cargoHash = lib.fakeHash;
+
+  nativeBuildInputs = [
+    cargo-tauri.hook
+    pkg-config
+    # Setup a JS Frontend using npm, yarn, pnpm & bun (Coming soon)
+  ];
+
+
+    buildInputs =
+    [ openssl ]
+    ++ lib.optionals stdenv.isLinux [
+      glib-networking # Most Tauri apps need networking
+      libsoup
+      webkitgtk
+    ]
+    ++ lib.optionals stdenv.isDarwin (
+      with darwin.apple_sdk.frameworks;
+      [
+        AppKit
+        CoreServices
+        Security
+        WebKit
+      ]
+    );
+
+  meta = with lib; {
+    description = "";
+    homepage = "url";
+    license = licenses.unlicense;
+    maintainers = [];
+  };
+}
